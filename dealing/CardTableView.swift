7719c9c749a36e9eeb9d3915cabd85d7ba71a315
Tina <nancy.catty@gmail.com>
2019-06-10 23:00:05 +0200

    Dealing out card successfully

--- dealing/CardTableView.swift
+++ dealing/CardTableView.swift
@@ -10,62 +10,85 @@ import UIKit
 
 class CardTableView: UIView {
     
+    lazy var viewFrame = self.frame
+    
     enum ViewName: Int {
         case NewGame = 1
         case Deal = 2
         case Set = 3
+        case CardTable = 4
     }
     
     lazy var cardButtons = [CardView]()
     var numberOfCardsOnTable = 0
-    lazy var dealView = viewWithTag(ViewName.Deal.rawValue)
-    lazy var newGameView = viewWithTag(ViewName.NewGame.rawValue)
-
     var grid = Grid(layout: .aspectRatio(2))
     
-    func getTableArea() -> CGRect
-    {
-        let newGameViewFrame = newGameView!.frame
-        return CGRect(x: frame.minX, y: newGameViewFrame.maxY+5, width: newGameViewFrame.width, height: (dealView!.frame.minY - newGameViewFrame.maxY)*0.9)
-    }
-    
     func addCardButton(amount: Int) {
+        let dealFrame = self.viewWithTag(ViewName.Deal.rawValue)!.frame
         grid.cellCount = numberOfCardsOnTable + amount
         for index in 0..<amount {
-            let button = CardView.init(frame: grid[numberOfCardsOnTable +  index]!.insetBy(dx: 10, dy: 10))
-            button.alpha = 0.5
+            let newCell = grid[numberOfCardsOnTable +  index]!.insetBy(dx: 10, dy: 10)
+            let button = CardView.init(frame: newCell)
+            button.number = numberOfCardsOnTable + index
+            button.backgroundColor = UIColor.purple
             cardButtons.append(button)
             addSubview(button)
+            button.alpha = 0
+            layoutIfNeeded()
+            UIViewPropertyAnimator.runningPropertyAnimator(
+                withDuration: 0, delay: 0, options: .curveEaseIn,
+                animations: {
+                    button.frame = dealFrame
+                    
+            }
+                ,completion: { _ in
+                    UIViewPropertyAnimator.runningPropertyAnimator(
+                        withDuration: 0.5, delay: 0.4, options: [],
+                        animations: {
+                            button.alpha = 1;
+                            button.frame = newCell
+                    },
+                        completion: { _ in
+                            
+                            UIView.transition(with: button, duration: 0.5, options: .transitionFlipFromLeft, animations: {button.isFaceUp = true})
+                    }
+                    )
+            }
+            )
         }
         numberOfCardsOnTable += amount
-        setNeedsLayout()
     }
-
+    
     
     // Remove all subviews
-    func clearTable() {
-//        print("viewFrame.maxX: " + self.frame.maxX.description)
+    func ClearTable() {
+        //        print(description)
         numberOfCardsOnTable = 0
         cardButtons.forEach{$0.removeFromSuperview()}
+        cardButtons.removeAll()
+        
     }
     
     override func layoutSubviews() {
+        print("layoutSubviews")
         super.layoutSubviews()
-        let tableArea = getTableArea()
-        print("layoutSubviews.tableArea.maxX: " + tableArea.maxX.description)
-        grid.frame = tableArea
+        updateSubviews()
+        //        if (self.frame != viewFrame) {
+        //            updateSubviews()
+        //            viewFrame = self.frame
+        //        }
+    }
+    
+    private func updateSubviews() {
+        print("updateSubViews")
+        let tableArea = self.viewWithTag(ViewName.CardTable.rawValue)
+        grid.frame = tableArea!.frame
         if (!cardButtons.isEmpty) {
             for index in 0..<numberOfCardsOnTable {
                 cardButtons[index].frame = grid[index]!.insetBy(dx: 10, dy: 10)
                 cardButtons[index].number = index
                 cardButtons[index].backgroundColor = UIColor.purple
             }
-            let v = UIView.init(frame: grid[0]!)
-            v.backgroundColor = UIColor.orange
-            self.addSubview(v)
-//            print("grid[0].maxX: " + grid[0]!.maxX.description)
-//            print("CardButtons[0].maxX: " + cardButtons[0].frame.maxX.description)
         }
-        
     }
 }
--- dealing/ViewController.swift
+++ dealing/ViewController.swift
@@ -21,71 +21,19 @@ class ViewController: UIViewController {
     lazy var cardBehavior = CardBehavior(in: animator)
     
     @IBAction func ClickNewGame(_ sender: UIButton) {
-         print(description)
-        CardTableView.clearTable()
+        CardTableView.ClearTable()
         CardTableView.addCardButton(amount: nrOfInitialCards)
-        CardTableView.grid.cellCount = CardTableView.cardButtons.count
-      //  AnimateDealOneCard(to: CardTableView.grid[0]! )
+//        AnimateDealOneCard(card: CardTableView.cardButtons[0])
         //AnimateDealCard(amount: nrOfInitialCards)
     }
     
     @IBAction func ClickDeal(_ sender: UIButton) {
         CardTableView.addCardButton(amount: 3)
+        
     //    AnimateDealOneCard(to: CardTableView.cardButtons[4].frame)
     }
-    
-    private func AnimateDealOneCard(to dest: CGRect) {
-      //  print(dest.description)
-        let animateCard = CardView.init(frame: DealButton.frame)
-        animateCard.backgroundColor = UIColor.black
-        view.addSubview(animateCard)
-        UIViewPropertyAnimator.runningPropertyAnimator(
-            withDuration: 0.5, delay: 1, options: [],
-            animations: {
-                animateCard.frame = dest
-                animateCard.alpha = 0.2
-        }
-//            , completion:{
-//                _ in
-//                UIViewPropertyAnimator.runningPropertyAnimator(
-//                    withDuration: 0.5, delay: 7, options:[], animations: {
-//                        UIView.transition(
-//                            with: dest,
-//                            duration: 0.3,
-//                            options: .transitionFlipFromRight,
-//                            animations: {
-//                                dest.isFaceUp = true
-//                                dest.alpha = 1
-//                        },
-//                            completion: {
-//                                _ in
-//                                animateCard.removeFromSuperview()
-//                        }
-//                        )
-//                }
-//                )
-//        }
-        )
-        
-//        self.cardBehavior.Snap(item: animateCard, dest: dest.center)
-////        animateCard.alpha=0
-//        CardView.transition(with: dest, duration: 1, options: .transitionFlipFromLeft, animations: {dest.isFaceUp = true}//, completion: {_ in animateCard.removeFromSuperview()}
-//        )
-       
-    }
-    
-    
-    private func AnimateDealCard(amount: Int) {
-        let cardButtons = CardTableView.cardButtons
-        AnimateDealOneCard(to: cardButtons[0].frame )
-        // create a subview with the same size of Deal button
-        // snap it to the destination
-        // show the card
-      //  CardTableView.displayCard(index: 0)
-        // flip card to face up
-    }
-    
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
